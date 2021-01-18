18c69f89896c57d816e23fe93b5083f2ac8e0cd5
jerlendds <jerlendds@gmail.com>
2021-01-17 19:39:19 -0700

    testing

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# ISAR - Intelligent Search & Retrieve
-After [PhotoRec](https://github.com/cgsecurity/testdisk) file recovery tool. ISAR let's you query a folder with `recup_dir` directories for strings contained in files and can attempt to rebuild the lost folder structure or return the results by filetype. Results can be organized by what folder structure can be rebuilt and also by date and or detected format. 
+# ISAR - Illuminate Search & Retrieve
+After [PhotoRec](https://github.com/cgsecurity/testdisk) file recovery tool for linux. ISAR let's you query a folder with `recup_dir` directory files by strings of your choice and can attempt to detect lost filetypes. Results can be organized by any found metadata and filetypes. 
  > “Things are never lost to you; you are lost to them. If ever in need of a Thing that has lost you, simply stop hiding from it.” 
 
 
 ## Motivation
-While setting up a headless CMS system I generated some files in the wrong directory and went to rm them. I mistyped something and deleted all the folders on my system where I lost 60% of all code I wrote in the past year that wasn't in the cloud. Lessons have been learned, use backups and version control religiously but I'm determined to recover atleast some of my code hence the birth of this tool. Development started on the morning of January 17, 2021.
+While setting up a headless CMS system I generated some files in the wrong directory and went to rm them. I mistyped something and deleted all the folders on my system where I lost 60% of all code I wrote in the past year that wasn't in the cloud. Lessons have been learned, use backups and version control religiously but I'm determined to recover atleast some of my code hence the start of this tool.
 
 
-## Features
+## Features (NOT IMPLEMENTED)
  - Query for strings in files
  - File type detection
  - Sort results by date
@@ -16,11 +16,11 @@ While setting up a headless CMS system I generated some files in the wrong direc
 
 
 ## Installation
-*Sorry, this project isn't ready for the public yet.* 
+*Sorry, this project isn't ready yet.* 
 
 
 ## How to use?
-*Sorry, this project isn't ready for the public yet.* 
+*Sorry, this project isn't ready yet.* 
 
 
 ## License
---
+++ main.py
@@ -0,0 +1,67 @@
+testing_paths = ["/media/jerlends/fb21cc72-c5c3-4486-855b-fd52e9e0a272"]
+
+
+import os
+
+def scan_for_directories(directories):
+    """Yield all recup_dir directories in passed list in format {"dirint": int, "path": str}
+
+    Args:
+        directories (list): list of path(s) to directory that contains recup_dir folders
+    """
+    for path in directories:
+        path_str = path
+        for entry in os.scandir(path_str):
+            # * basename provides everything after last slash
+            cleaned_path = os.path.basename(entry.path).split(".")[1]
+            try:
+                cleaned_path = int(cleaned_path)
+                yield {"dirint": cleaned_path, "path": entry.path}
+            except ValueError:
+                print("Unexpected directory format:", cleaned_path)
+                if (
+                    input(
+                        f'Do you want to include "{cleaned_path}" in the search? (y/n)'
+                    ).lower()
+                    == "y"
+                ):
+                    yield {"dirint": cleaned_path, "path": entry.path}
+
+
+def sort_directories(directories, is_descending=False):
+    """Yield all directories in ascending order
+
+    Args:
+        directories (list): path string(s) to directory that contains recup_dir folders
+    [Optional]:
+        is_decending (bool): if true yield descending
+    """
+    directory_generator = scan_for_directories(directories)
+    mylist = []
+    for directory in directory_generator:
+        mylist.append(directory)
+    sorted_list = sorted(mylist, key=lambda k: k["dirint"], reverse=is_descending)
+    yield from sorted_list
+
+
+def scan_for_files(directory):
+    for path in os.listdir(directory):
+        yield os.path.join(directory, path)
+
+
+def recover_metadata(directories):
+    """Recover metadata from iterable"""
+    count = 0
+    for directory in directories:
+        count += 1
+        for current_file in scan_for_files(directory["path"]):
+            print(current_file)
+            with open(current_file, "rb") as f:
+                first_line = f.readline()
+                if first_line[:4] == "file:///":
+                    print(f"Metadata for {count} files found:", first_line[:40])
+
+
+
+if __name__ == "__main__":
+    print("Sorry, this software isn't ready for release yet. If you're interested in contributing please fork and make a PR. Thanks, Jordan")
