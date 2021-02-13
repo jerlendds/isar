2f35c53be698e3099b60d477c983cbd9a4101780
jerlendds <jerlendds@gmail.com>
2021-02-12 17:06:15 -0700

    Added: parsing meta data into dictionary

--- README.md
+++ README.md
@@ -7,12 +7,8 @@ After [PhotoRec](https://github.com/cgsecurity/testdisk) file recovery tool for 
 While setting up a headless CMS system I generated some files in the wrong directory and went to rm them. I mistyped something and deleted all the folders on my system where I lost 60% of all code I wrote in the past year that wasn't in the cloud. Lessons have been learned, use backups and version control religiously but I'm determined to recover atleast some of my code hence the start of this tool.
 
 
-## Features (NOT IMPLEMENTED)
- - Query for strings in files
- - File type detection
- - Sort results by date
- - Sort results by file format
- - Rebuild lost folder structure from found metadata
+## Features
+ - Rebuild lost directory structure/files from found metadata
 
 
 ## Installation
--- main.py
+++ main.py
@@ -1,61 +1,30 @@
-"""This file is under construction!"""
-
-
-
 import os
-import re
-import time
 
-testing_paths = [
-    "/media/jerlends/Samsung_T5/Data Recovery/",
+# import ipdb
+
+source_paths = [
     "/media/jerlends/fb21cc72-c5c3-4486-855b-fd52e9e0a272",
+    "/media/jerlends/Samsung_T5/Data Recovery/",
 ]
 
-if __name__ == "__main__":
-    print(
-        f"Sorry, this software isn't ready for release yet. If you're interested in contributing please fork and make a PR. Thanks \n\n To recover metadata use the recover_metadata function and pass in the sort directories function with testing_paths. \n\n\n ATTEMPTING META DATA RECOVERY FROM {testing_paths}"
-    )
-    time.sleep(8)
 
 def scan_for_directories(directories):
-    """Yield all recup_dir directories in passed list in format {"dirint": int, "path": str}
+    """Yield all recup_dir directories in passed list
+        in format {"dirint": int, "path": str}
 
     Args:
-        directories (list): list of path(s) to directory that contains recup_dir folders
+        directories (list): list of path(s) to recup_dir folders
     """
     for path in directories:
         path_str = path
         for entry in os.scandir(path_str):
-            # * basename provides everything after last slash
-            cleaned_path = os.path.basename(entry.path).split(".")[1]
             try:
-                cleaned_path = int(cleaned_path)
-                yield {"dirint": cleaned_path, "path": entry.path}
+                yield entry.path
             except ValueError:
-                print("Unexpected directory format:", cleaned_path)
-                if (
-                    input(
-                        f'Do you want to include "{cleaned_path}" in the search? (y/n)'
-                    ).lower()
-                    == "y"
-                ):
-                    yield {"dirint": cleaned_path, "path": entry.path}
+                print("Unexpected directory format")
 
 
-def sort_directories(directories, is_descending=False):
-    """Yield all directories in ascending order
-
-    Args:
-        directories (list): path string(s) to directory that contains recup_dir folders
-    [Optional]:
-        is_decending (bool): if true yield descending
-    """
-    directory_generator = scan_for_directories(directories)
-    mylist = []
-    for directory in directory_generator:
-        mylist.append(directory)
-    sorted_list = sorted(mylist, key=lambda k: k["dirint"], reverse=is_descending)
-    yield from sorted_list
+files_list = ["file:///", "b'file:///", "b'file:///'"]
 
 
 def scan_for_files(directory):
@@ -66,36 +35,33 @@ def scan_for_files(directory):
 def get_files(directories):
     """Recover metadata from files"""
     for directory in directories:
-        yield from scan_for_files(directory["path"])
-
-
-files_list = ["file:///", "b'file:///", "b'file:///'"]
+        yield from scan_for_files(directory)
 
 
 def recover_metadata(directories):
-    metadata_files_count = 0
     for each_file in get_files(directories):
         with open(each_file, "rb") as f:
             first_line = f.readline()
             detect_meta = str(first_line[:8])
             if detect_meta in files_list:
-                metadata_files_count += 1
-                print(
-                    metadata_files_count,
-                    "\n",
-                    first_line,
-                    "\n\n",
-                    each_file,
-                    "\n\n\n\n",
-                )
+                # noqa   index 0 = unknown file, index 1 = name of unknown file
+                yield (each_file, first_line)
 
 
-recover_metadata(sort_directories(testing_paths, True))
+def parse_metadata(old_files_current_location):
+    for each_file in old_files_current_location:
+        files = each_file[1][22:].decode().split('/')
+        meta_time = files[-1].split(' ')[-1].split(',')
+        mtime = meta_time[0].replace("{", "")
+        mtime = mtime.replace('"mtime":', '')
+        ctime = meta_time[1]
+        ctime = ctime.replace('"ctime":', '')
+        clean_last_element = files[-1].split(' ')
+        files.pop(-1)
+        files.append(clean_last_element[0])
+        files = "/".join(files)
+        yield { "lost_name": files, "mtime": mtime, "ctime": ctime, "current_location": each_file[0] } # noqa
 
 
-def sort_metadata():
-    """Remove known system files and organize by mtime or ctime"""
-    pass
-
-
-
+for x in parse_metadata(recover_metadata(scan_for_directories(source_paths))):
+    print(x, '\n\n')
